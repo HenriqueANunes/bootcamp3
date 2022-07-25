0a617ea6018ff112cc009e5469ff645abbefd4da
hman <henrique.a.nunes@outlook.com>
2022-07-24 21:05:47 -0300

    muito perfeito na moral, pode dar 10 aqui

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function salvar(form){
         obj_coluna.title = coluna;
 
         if(coluna === 'check'){
-            obj_coluna.innerHTML = '<input type="checkbox">';
+            obj_coluna.innerHTML = '<input type="checkbox" onchange="toggle_remover(); toggle_check_all()">';
         }else if(coluna === 'id'){
             obj_coluna.innerText = ultimo_id+1;
         }
@@ -26,9 +26,10 @@ function salvar(form){
             obj_coluna.innerText = dados_linha[coluna];
         }
     }
+    form.reset()
 }
 
-function toggle_chebox(checkbox){
+function toggle_checkbox(checkbox){
     document.querySelectorAll('#table input[type=checkbox]').forEach((input)=>{
         input.checked = checkbox.checked;
     })
@@ -70,4 +71,37 @@ function buscar_cep(cep){
             console.log(error);
         }
     })
+}
+
+function toggle_remover(){
+    const btn_remover = document.getElementById('remover');
+
+    let desabilitar = true;
+    document.querySelectorAll('#table tbody input[type=checkbox]').forEach((input)=>{
+        if(input.checked){
+            desabilitar = false;
+        }
+    })
+
+    if(desabilitar){
+        btn_remover.setAttribute('disabled', '');
+    }else{
+        btn_remover.removeAttribute('disabled');
+    }
+}
+
+function toggle_check_all(){
+    const check_all = document.getElementById('check_all');
+    const lista_checkboxes = document.querySelectorAll('#table tbody input[type=checkbox]');
+
+    let desabilitar = true;
+    lista_checkboxes.forEach((input)=>{
+        if(!input.checked){
+            desabilitar = false;
+        }
+    })
+    if(lista_checkboxes.length === 0){
+        desabilitar = false;
+    }
+    check_all.checked = desabilitar;
 }
